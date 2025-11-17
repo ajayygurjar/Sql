1aae36c62bacb32aeec083cc6648e940019f9969
ajayygurjar <ajay1651@gmail.com>
2025-11-17 12:56:40 +0530

    Making SQL Easy with Sequelize

--- index.js
+++ index.js
@@ -2,13 +2,18 @@ const express = require("express");
 const app = express();
 const db = require("./utils/db-connection");
 const studentRoutes = require("./routes/studentsRoutes");
-
+const studentModels=require('./models/students')
 app.use(express.json());
 app.get("/", (req, res) => {
   res.send("Hello world");
 });
 app.use("/students", studentRoutes);
-
+db.sync({force:true}).then(()=>{
 app.listen(3000, (err) => {
   console.log("Server is running");
 });
+
+
+}).catch((err)=>{
+  console.log(err)
+})
--- utils/db-connection.js
+++ utils/db-connection.js
@@ -1,35 +1,79 @@
+const {Sequelize}=require('sequelize');
 
-const mysql = require('mysql2');
-
-const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'Ajay@2529',
-  database: 'testDB'
+const sequelize=new Sequelize('testdb','root','Ajay@2529',{
+  host:'localhost',
+  dialect:'mysql'
 });
 
-connection.connect((err) => {
-  if (err) {
-    console.error("Database connection failed:", err);
-    return;
-  }
-  console.log(`Connection has been created`);
 
-  const creationQuery = `CREATE TABLE IF NOT EXISTS Students(
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(20),
-    email VARCHAR(50) unique,
-    age int
-  )`;
+(async()=>{try {
+  await sequelize.authenticate();
+  console.log('Connection to database has been created')
+} catch (error) {
+  console.log(error)
+}})();
 
-  connection.execute(creationQuery, (err) => {
-    if (err) {
-      console.error("Table creation error:", err);
-      connection.end();
-      return;
-    }
-    console.log(`Students table is ready`);
-  });
-});
+module.exports=sequelize;
 
-module.exports = connection;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const mysql = require('mysql2');
+
+// const connection = mysql.createConnection({
+//   host: 'localhost',
+//   user: 'root',
+//   password: 'Ajay@2529',
+//   database: 'testDB'
+// });
+
+// connection.connect((err) => {
+//   if (err) {
+//     console.error("Database connection failed:", err);
+//     return;
+//   }
+//   console.log(`Connection has been created`);
+
+//   const creationQuery = `CREATE TABLE IF NOT EXISTS Students(
+//     id INT AUTO_INCREMENT PRIMARY KEY,
+//     name VARCHAR(20),
+//     email VARCHAR(50) unique,
+//     age int
+//   )`;
+
+//   connection.execute(creationQuery, (err) => {
+//     if (err) {
+//       console.error("Table creation error:", err);
+//       connection.end();
+//       return;
+//     }
+//     console.log(`Students table is ready`);
+//   });
+// });
+
+// module.exports = connection;
