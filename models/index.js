ceeee6fa887d824d9a196c8ea6a714c01bbc0cda
ajayygurjar <ajay1651@gmail.com>
2025-11-25 12:27:46 +0530

    many to many connection setup

--- models/index.js
+++ models/index.js
@@ -2,18 +2,23 @@
 const Student=require('./students');
 const IdentityCard=require('./identityCard');
 const department=require('./department');
+const courses=require('./courses')
+const studentCourses=require('./studentCourses')
 
-//one to one
+//one to one association
 
 Student.hasOne(IdentityCard);
 IdentityCard.belongsTo(Student);
 
-//one to many
+//one to many association
 
 department.hasMany(Student);
 Student.belongsTo(department);
 
+//many to many association
+Student.belongsToMany(courses,{through:studentCourses})
+courses.belongsToMany(Student,{through:studentCourses});
 
 module.exports={
-    Student,IdentityCard
+    Student,IdentityCard,courses,studentCourses
 }
